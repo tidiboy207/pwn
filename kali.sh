7423fb1ee352c2a152ae9ff852a2e658a82f3f67
tidiboy207 <duccoi4321@gmail.com>
2025-07-26 21:37:54 +0700

    v2

--- kali.sh
+++ kali.sh
@@ -1,2 +1,3 @@
 #Add alias ida64 to kali
 echo "alias ida64='/mnt/c/Users/ASUS/Downloads/IDA\ 7.7\ all\ decompiler/IDA\ 7.7/ida64.exe'" >> ~/.zshrc
+echo "alias ida='/mnt/c/Users/ASUS/Downloads/IDA\ 7.7\ all\ decompiler/IDA\ 7.7/ida.exe'" >> ~/.zshrc
